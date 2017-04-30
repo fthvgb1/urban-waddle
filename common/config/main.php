47d963fec3243bacccc4f32e1492b1f85bfc42fc
fthvg <fthvgb1@163.com>
2017-04-30 22:27:22 +0800

    时间格式化

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,12 @@
 <?php
 return [
     'language'=> 'zh-cn',
-
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'formatter' => [
+            'dateFormat' => 'yyyy-MM-dd',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
